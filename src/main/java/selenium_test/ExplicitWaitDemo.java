ca9577be01dc76372b01c93beacc53e403509455
skmaha <sunil4356@gmail.com>
2022-07-27 14:00:01 +0200

    Just practice explicitwait

---
+++ src/main/java/selenium_test/ExplicitWaitDemo.java
@@ -0,0 +1,45 @@
+package selenium_test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWaitDemo {
+
+    public static void main(String[] args) {
+
+		// Start browser
+        System.setProperty("webdriver.gecko.driver", "/Users/sunilk/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+
+		// Start application
+		driver.get("http://seleniumpractise.blogspot.in/2016/08/how-to-use-explicit-wait-in-selenium.html");
+
+		// Click on timer button to start
+		driver.findElement(By.xpath("//button[text()='Click me to start timer']")).click();
+
+		// Create object of WebDriverWait class and it will wait max of 20 seconds. By default it will accepts in Seconds
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+		// Here we will wait until element is not visible, if element is visible then it will return web element or else it will throw exception
+		WebElement element = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='WebDriver']")));
+
+		// if element found then we will use- In this example I just called isDisplayed method
+		boolean status = element.isDisplayed();
+
+		// if else condition
+		if (status) {
+			System.out.println("===== WebDriver is visible======");
+		} else {
+			System.out.println("===== WebDriver is not visible======");
+		}
+
+	}
+    
+}
--- src/main/java/selenium_test/waits.java
+++ src/main/java/selenium_test/waits.java
@@ -1,24 +1,51 @@
 package selenium_test;
 
-import java.sql.Driver;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver.Timeouts;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import net.bytebuddy.agent.builder.AgentBuilder.RedefinitionStrategy.DiscoveryStrategy.Explicit;
 
 public class waits {
 
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/Users/sunilk/chromedriver");
-		WebDriver driver = new ChromeDriver();
-
-    driver.get("https://www.hyrtutorials.com/p/waits-demo.html");
-    driver.findElement(By.cssSelector("#btn2")).click();
-//https://www.seleniumeasy.com/selenium-tutorials/waits-and-timeout-selenium-4
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    driver.findElement(By.xpath("(//input[@id='txt2'])[1]")).sendKeys("This is Cool-2");
-    Thread.sleep(5000);
-    driver.quit();
+        explicitWait();
+        //implicitlyWait();
     }
+    
+    // best explanation by pramod https://www.youtube.com/watch?v=upDhIhDezxQ
+    public static void explicitWait() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/sunilk/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com/ncr");
+        driver.findElement(By.xpath("//*[@id='L2AGLb']")).click(); // to accept the cookies "Accept all" button click
+        //driver.findElement(By.xpath("input[title='Search']")).sendKeys("Cheese" + Keys.ENTER);
+        driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+        WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(20))
+        .until(ExpectedConditions.elementToBeClickable(By.xpath("//a/h3")));
+        ((Timeouts) driver.manage()).implicitlyWait(Duration.ofSeconds(10));
+        System.out.println(firstResult.getText());
+    }
+
+    //https://www.seleniumeasy.com/selenium-tutorials/waits-and-timeout-selenium-4
+    public static void implicitlyWait() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/sunilk/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hyrtutorials.com/p/waits-demo.html");
+        driver.findElement(By.cssSelector("#btn2")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.xpath("(//input[@id='txt2'])[1]")).sendKeys("This is Cool-2");
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
 }
